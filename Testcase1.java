4422866251857916c1dd6cca08b36e941b2d3763
Ifrah anjum M <Ifrahanjum3004@gmail.com>
2026-09-09 12:53:18 +0530

    Add files via upload

---
+++ Testcase1.java
@@ -0,0 +1,39 @@
+package Assessments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Testcase1 {
+	public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().window().maximize();
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+            driver.get("https://www.facebook.com/");
+            WebElement createAccount = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(.,'Create new account')]")));
+            createAccount.click();
+            WebElement firstName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("R_1cl2p4jikacppb6amH1")));
+            WebElement surname = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("r_a")));
+            Point firstNameLocation = firstName.getLocation();
+            Point surnameLocation = surname.getLocation();
+            int firstNameY = firstNameLocation.getY();
+            int surnameY = surnameLocation.getY();
+            if (Math.abs(firstNameY - surnameY) <= 5) {
+                System.out.println("TEST CASE PASSED");
+            } else {
+                System.out.println("TEST CASE FAILED");
+            }
+        } catch (Exception e) {
+            System.out.println("TEST CASE FAILED");
+        }
+    }
+}
+
